882ff7badd3c9bc341b7c0e816e9e1af87d03e71
mctyn64 <bneuman@Bens-MacBook-Air.local>
2014-01-31 13:51:29 -0600

    Divided controllers Divided controllers between user.rb, round.rb, and index.rb

---
+++ app/controllers/round.rb
@@ -0,0 +1,22 @@
+get '/round/question/'
+  #max_guess is stored in cookie.
+  #@card = Card.random based on current deck. Current deck is stored in cookie.
+  #We also check guesses to make sure we don't repeat cards you have answere correctly.
+  #Round_id is also stored in cookie, because it would be weird to expose via URL.
+  #current_card_id is also stored in cookie so we know what answer to look up.
+  #'submit guess' sends POST to /round/answer/ with your guess
+end
+
+post '/round/answer/'
+  #Post logs your guess and its correctness.
+  #It then redirects to /round/display_answer/ with the guess_id stored in query string.
+
+end
+
+get '/round/display_answer/'
+  #Retrives your guess based on guess_id in query string.
+  #Displays your guess, the answer, and correctness.
+  #Check number of guesses made vs. max_guesses in round.
+    #If done, render with 'done' link.
+    #Else, render with 'next question' link.
+end
