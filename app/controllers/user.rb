e000bcfc25621f900574c45789cebda52857d8d5
Apprentice <apprentice@dbc-chicago-83.local>
2014-01-31 15:52:01 -0600

    removed trailing slashes from routes

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,23 +1,19 @@
-post '/login/' do
+post '/login' do
 #Validates username/password. Redirects to /decks if successful.
   # params[:login].inspect
   user_id = User.login(params[:login])
   if user_id > 0
     session[:user_id] = user_id
-    redirect to '/decks/'
+    redirect to '/decks'
   else
     redirect "/"
   end
 end
 
-get '/users/:user_id/' do
+get '/users/:user_id' do
 #This is the 'profile' page. Displays history of rounds played.
 end
 
-get '/users/new/' do
-  #New user page - form with name, password, email, etc.
-  #'Save' POST to /users/create/
-end
 
 post '/users/create' do
   #Generates user from form input.
@@ -26,7 +22,7 @@ post '/users/create' do
   new_user = params[:new_user]
   user = User.create(user_name: new_user[:user_name], password: new_user[:password], email: new_user[:email], name: new_user[:name])
   session[:user_id] = user.id
-  redirect to '/decks/'
+  redirect to '/decks'
 end
 
 get '/users/create' do
@@ -34,18 +30,18 @@ get '/users/create' do
   erb :create_user
 end
 
-get '/users/logout/' do
+get '/users/logout' do
   #Logs user out.
   #Redirects to 'login' /
   session[:user_id] = 0
 end
 
-get '/login/error/' do
+get '/login/error' do
   #Where you are redirected when POST /login route has problem with your info.
   #Takes your info as query string so we can display it + error
 end
 
-get '/signup/error/' do
+get '/signup/error' do
   #Where you are redirected when POST /users/create route has problem with your info.
   #Takes your info as query string so we can display it + error
 end
